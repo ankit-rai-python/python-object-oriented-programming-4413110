e38270c0c740c8200ff27ef3a28188f6b51b8c9a
Ankit Rai <133601149+ankitrai1985@users.noreply.github.com>
2024-12-24 19:38:58 +0000

    added

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,13 @@
 
 
 # TODO: create a basic class
-
+class Book:
+  def __init__(self,title):
+    self.title = title
 
 # TODO: create instances of the class
-
+book1 = Book("Wings of fire")
 
 # TODO: print the class and property
+print(f'Object', book1)
+print('title : ', book1.title)
